94f4bbcc372836a173740b50d5606d7239fd89b1
Klim Bodrov <klim.hin.bo@gmail.com>
2025-03-29 12:54:28 +0300

    почти ничего нового, ебуст с отчисткой

--- countsym/views.py
+++ countsym/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def count(request):
+    sumb = list()
+    a = 0
+    text1 = request.GET.get('type')
+    a = text1.replace(('!"№;%:?*()@#$^&'), '')
+    print(a)
     return render(request, 'countsym/countsumbol.html')
-    a = list()
-    if request.GET.get('comment') != 0:
-        pass
